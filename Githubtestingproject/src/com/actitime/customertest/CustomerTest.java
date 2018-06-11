cd2b7e51cb8b95c55c2b15a7450ceb317c50db79
Doddapaneni <Doddapaneni@Doddapaneni-PC>
2018-06-11 21:01:26 +0530

    Commit 1

---
+++ Githubtestingproject/src/com/actitime/customertest/CustomerTest.java
@@ -0,0 +1,14 @@
+package com.actitime.customertest;
+
+import org.testng.annotations.Test;
+
+public class CustomerTest {
+	
+	@Test
+	
+	public void createCustomer()
+	{
+		System.out.println("************Customer Test**************");
+	}
+
+}
